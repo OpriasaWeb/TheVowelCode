9c5c9efda898add14458cc4bdc23e7ca5c2e3be7
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-06-12 19:27:27 +0800

    feat: The Vowel Code (6 kyu challenge)

---
+++ vowelCode.js
@@ -0,0 +1,65 @@
+// June 12, 2023
+// CodeWars: The Vowel Code (6 kyu challenge)
+// Description: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern. Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown below.
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+
+function encode(string) {
+
+  var vowels = {
+    a: "1",
+    e: "2",
+    i: "3",
+    o: "4",
+    u: "5"
+  }
+  
+  var encodedString = "";
+  
+  for(let i = 0; i < string.length; i++){
+    
+    let currentLetter = string[i];
+    let encodeChar = vowels[currentLetter] || currentLetter;
+    
+    encodedString += encodeChar;
+ 
+    console.log(encodedString);
+    
+  }
+  
+  return encodedString;
+}
+
+encode('hello')
+
+
+function decode(string) {
+  var counterVowels = {
+    1: "a",
+    2: "e",
+    3: "i",
+    4: "o",
+    5: "u"
+  }
+  
+  var decodeString = "";
+  
+  for(let i = 0; i < string.length; i++){
+    let currentDecode = string[i];
+    let passedDecode = counterVowels[currentDecode] || currentDecode;
+    
+    decodeString += passedDecode;
+    
+    console.log(decodeString);
+    
+  }
+  
+  return decodeString;
+
+}
+
+decode('h2ll4')
